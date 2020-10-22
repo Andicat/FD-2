88a17ebc9d9d2dfc51e0ea99b97a72ea6685d051
Ekaterina <andikatya@gmail.com>
2020-10-22 11:00:07 +0300

    dyn-form

--- dyn_form.js
+++ dyn_form.js
@@ -22,86 +22,91 @@ N.23 Домашнее задание DYN_FORM
         return;
     }
 
-    var formElemTypes = {
-        longtext: "text",
-        number: "number",
-        shorttext: "text",
-        combo: "select",
-        radio: "radio",
-        check: "checkbox",
-        memo: "textarea",
-        submit: "submit",
-    };
-
     //строим форму динамически
-    function createForm(form,elemArr) {
-        console.log(form.elements);
-        console.log(elemArr);
+    function createForm(form,elemArr, formName) {
+        form.setAttribute("name",formName);
+        form.setAttribute("action","https://fe.it-academy.by/TestForm.php");
+        form.setAttribute("method","post");
+               
         elemArr.forEach( function(el) {
             switch(el.kind) {
                 case "longtext":
                     var fieldset = document.createElement("fieldset");
+                    
                     var label = document.createElement("label");
                     label.textContent = el.label;
                     label.setAttribute("for",el.name);
+                    fieldset.appendChild(label);
+
                     var input = document.createElement("input");
                     input.setAttribute("type","text");
-                    input.setAttribute("id",el.name);
                     input.setAttribute("name",el.name);
-                    fieldset.appendChild(label);
+                    input.setAttribute("id",el.name);
                     fieldset.appendChild(input);
+                    
                     form.appendChild(fieldset);
                     break;
                 case "shorttext":
                     var fieldset = document.createElement("fieldset");
+                    
                     var label = document.createElement("label");
                     label.textContent = el.label;
                     label.setAttribute("for",el.name);
+                    fieldset.appendChild(label);
+
                     var input = document.createElement("input");
                     input.setAttribute("type","text");
-                    input.setAttribute("class",el.kind);
-                    input.setAttribute("id",el.name);
                     input.setAttribute("name",el.name);
-                    fieldset.appendChild(label);
+                    input.setAttribute("id",el.name);
+                    input.setAttribute("class",el.kind);
                     fieldset.appendChild(input);
+                    
                     form.appendChild(fieldset);
                     break;
                 case "number":
                     var fieldset = document.createElement("fieldset");
+
                     var label = document.createElement("label");
                     label.textContent = el.label;
                     label.setAttribute("for",el.name);
+                    fieldset.appendChild(label);
+
                     var input = document.createElement("input");
                     input.setAttribute("type","number");
-                    input.setAttribute("id",el.name);
                     input.setAttribute("name",el.name);
-                    fieldset.appendChild(label);
+                    input.setAttribute("id",el.name);
                     fieldset.appendChild(input);
+                    
                     form.appendChild(fieldset);
                     break;
                 case "combo":
                     var fieldset = document.createElement("fieldset");
+                    
                     var label = document.createElement("label");
                     label.textContent = el.label;
                     label.setAttribute("for",el.name);
+                    fieldset.appendChild(label);
+
                     var select = document.createElement("select");
-                    select.setAttribute("id",el.name);
                     select.setAttribute("name",el.name);
+                    select.setAttribute("id",el.name);
                     el.variants.forEach( function(v) {
                         var option = document.createElement("option");
                         option.textContent = v.text;
                         option.setAttribute("value",v.value);
                         select.appendChild(option);
                     });
-                    fieldset.appendChild(label);
                     fieldset.appendChild(select);
+
                     form.appendChild(fieldset);
                     break;
                 case "radio":
                     var fieldset = document.createElement("fieldset");
+
                     var legend = document.createElement("legend");
                     legend.textContent = el.label;
                     fieldset.appendChild(legend);
+                    
                     el.variants.forEach( function(v) {
                         var radio = document.createElement("input");
                         radio.setAttribute("type","radio");
@@ -109,12 +114,52 @@ N.23 Домашнее задание DYN_FORM
                         radio.setAttribute("id",v.value);
                         radio.setAttribute("value",v.value);
                         fieldset.appendChild(radio);
+                        
                         var label = document.createElement("label");
                         label.textContent = v.text;
                         label.setAttribute("for",v.value);
                         fieldset.appendChild(label);
                     });
+
                     form.appendChild(fieldset);
+                    break;
+                case "check":
+                    var fieldset = document.createElement("fieldset");
+                    
+                    var label = document.createElement("label");
+                    label.textContent = el.label;
+                    label.setAttribute("for",el.name);
+                    fieldset.appendChild(label);
+                    
+                    var check = document.createElement("input");
+                    check.setAttribute("type","checkbox");
+                    check.setAttribute("name",el.name);
+                    check.setAttribute("id",el.name);
+                    fieldset.appendChild(check);
+
+                    form.appendChild(fieldset);
+                    break;
+                case "memo":
+                    var fieldset = document.createElement("fieldset");
+                    
+                    var label = document.createElement("label");
+                    label.textContent = el.label;
+                    label.setAttribute("for",el.name);
+                    fieldset.appendChild(label);
+                    
+                    var textarea = document.createElement("textarea");
+                    textarea.setAttribute("name",el.name);
+                    textarea.setAttribute("id",el.name);
+                    textarea.setAttribute("rows","5");
+                    fieldset.appendChild(textarea);
+
+                    form.appendChild(fieldset);
+                    break;
+                case "submit":
+                    var button = document.createElement("button");
+                    button.setAttribute("type","submit");
+                    button.textContent = el.label;
+                    form.appendChild(button);
                     break;
                 default:
                     break;
@@ -135,18 +180,22 @@ N.23 Домашнее задание DYN_FORM
             variants:[{text:'бесплатное',value:1},{text:'платное',value:2},{text:'VIP',value:3}]},
             {label:'Разрешить отзывы:',kind:'check',name:'votes'},
             {label:'Описание сайта:',kind:'memo',name:'description'},
-            {label:'Опубликовать:',kind:'submit'},
+            {label:'Опубликовать',kind:'submit'},
         ];
         var formDef2 = [
             {label:'Фамилия:',kind:'longtext',name:'lastname'},
             {label:'Имя:',kind:'longtext',name:'firstname'},
             {label:'Отчество:',kind:'longtext',name:'secondname'},
             {label:'Возраст:',kind:'number',name:'age'},
-            {label:'Зарегистрироваться:',kind:'submit'},
+            {label:'Зарегистрироваться',kind:'submit'},
         ];
-        var form1 = document.createElement("form");
+        cntForm.innerHTML = "";
         
-        createForm(form1,formDef1);
+        var form1 = document.createElement("form");
+        createForm(form1,formDef1,"form1");
+
+        var form2 = document.createElement("form");
+        createForm(form2,formDef2,"form2");
     });
 
 })();
