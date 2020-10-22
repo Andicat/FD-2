90b32ac19c7693be95ae247a26bca6f2a3f676fd
Katerina Andreeva <andikatya@gmail.com>
2020-10-22 20:38:42 +0300

    menu

--- popup_menu.js
+++ popup_menu.js
@@ -51,55 +51,56 @@ http://fe.it-academy.by/Examples/popup_menu.png
 
     class Menu {
 
-        constructor(arr,cnt) {
-            this.arr = arr;
-            this.cnt = cnt;
-            console.log(this.createMenu);
-            this.nav = document.createElement("nav");
-            console.log(this.nav);
-            this.nav.classList.add("popup-menu__nav");
-            this.cnt.appendChild(this.nav);
-        };
-        
-        createMenu = function (menuElemArr,cnt,isSubmenu) {
-            var ul = document.createElement("ul");
-            ul.classList.add("popup-menu__list");
-            if (isSubmenu) {
-                ul.classList.add("popup-menu__list--hidden"); 
+      constructor(arr,cnt) {
+        this.arr = arr;
+        this.cnt = cnt;
+      };
+      
+      createMenu = function () {
+
+        function create (menuElemArr,cnt,isSubmenu) {
+          var ul = document.createElement("ul");
+          ul.classList.add("popup-menu__list");
+          if (isSubmenu) {
+              ul.classList.add("popup-menu__list--sub"); 
+          }
+          menuElemArr.forEach( function(el) {
+            var li = document.createElement("li");
+            li.classList.add("popup-menu__item");
+            if (el.url) {
+                var link = document.createElement("a");
+                link.setAttribute("href",el.url);
+                link.textContent = el.name;
+                li.appendChild(link);
             }
-            menuElemArr.forEach( function(el) {
-                var li = document.createElement("li");
-                li.classList.add("popup-menu__item");
-                if (el.url) {
-                    var link = document.createElement("a");
-                    link.setAttribute("href",el.url);
-                    link.textContent = el.name;
-                    li.appendChild(link);
-                }
-                if (el.submenu) {
-                    li.textContent = el.name;
-                    debugger;
-                    this.createMenu(el.submenu,li,true);
-                }
-                ul.appendChild(li);
-            });
-            cnt.appendChild(ul);
+            if (el.submenu) {
+                li.textContent = el.name;
+                create(el.submenu,li,true);
+                li.addEventListener("mousedown", function(el) {
+                });
+            }
+            ul.appendChild(li);
+          });
+          cnt.appendChild(ul);
         };
+
+        create(this.arr,this.cnt);
+      }
     }
 
     btnCreate.addEventListener('click', (event) => {
         var menu = [
             {name:'Пункт 1',submenu:
-                [
-                    {name:'Пункт 1.1',submenu:
-                        [
-                        {name:'Пункт 1.1.1',url:'http://www.tut.by'},
-                        {name:'Пункт 1.1.2 длинный',url:'http://www.tut.by'}
-                        ]
-                    },
-                    {name:'Пункт 1.2',url:'http://www.tut.by'},
-                    {name:'Пункт 1.3 длинный',submenu:
-                [
+              [
+                {name:'Пункт 1.1',submenu:
+                  [
+                    {name:'Пункт 1.1.1',url:'http://www.tut.by'},
+                    {name:'Пункт 1.1.2 длинный',url:'http://www.tut.by'}
+                  ]
+                },
+                  {name:'Пункт 1.2',url:'http://www.tut.by'},
+                  {name:'Пункт 1.3 длинный',submenu:
+              [
                 {name:'Пункт 1.3.1',url:'http://www.tut.by'},
                 {name:'Пункт 1.3.2',url:'http://www.tut.by'},
                 {name:'Пункт 1.3.3',url:'http://www.tut.by'},
