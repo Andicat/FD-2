6fa513302b38b7058531b929a85a5c19c1667f1a
Ekaterina <andikatya@gmail.com>
2020-11-13 14:54:47 +0300

    clock

--- js/tests.js
+++ js/tests.js
@@ -188,6 +188,134 @@ console.log(+null)
 
 
     function test () {
+
+        /*Array.prototype.map2 = function(f) { 
+        var ar1 = this;
+        var ar2 = [];
+        for (var i = 0; i < ar1.length; i++) {
+            ar2.push(f(ar1[i],i,ar1));
+        }
+        return ar2;
+    };
+    var aaa = [1,2,3,4,5];
+    var bbb = aaa.map2(v=>v*2);
+    var f = function(v,i,a) { return "" + v + "" + i + "" + a};
+    console.log(aaa);
+    var ccc = aaa.map2(f);
+    console.log(bbb);
+    console.log(ccc);
+
+    var o = {"4":true};
+
+    console.log(o.hasOwnProperty("4"));
+    console.log(o);
+
+    eval("var x = 1;"); 
+    console.log(x);
+
+    var NaN = NaN
+    var undefined = undefined
+    var Infinity = "1"
+    var o = "ggg"
+    console.log(o);
+    var b;
+    
+    function ttt(a,a,a) {
+        var res = b = 3;
+    }
+    ttt();
+    console.log(res);*/
+
+    /*var a = "5";
+    var b = 2;
+    var c = a+++b;
+    console.log(c); //а+++b эквивалентно записи (a++)+b Значение - a равно "5", а значение - a++ равно 5 (так как это постфиксный инкремент) Следовательно а+++b = 5 + 2 = 7*/
+
+    //Оператор ~ равносилен - (n + 1) то есть - (2 + 1) = -3
+
+    let x;
+    x = 'hello' && 123; //x===123
+    x = 'hello' || 123; //x==='hello'
+    x = false && 123; //x===false
+    x = false || 123; //x===123
+
+    //палиндром
+    x = "abc";
+    x = x.toLowerCase().split("").reverse().join("");
+    //console.log(x);
+
+    /*
+    Напишите функцию, которая выводит в консоль числа от 1 до n, где n — целое число, которое функция принимает в качестве параметра, при этом:
+    выводит fizz вместо чисел, кратных 3;
+    выводит buzz вместо чисел, кратных 5;
+    выводит fizzbuzz вместо чисел, кратных и 3, и 5.
+    */
+    function fizzbuzz(value) {
+       var res;
+       for (var i=1; i<= value; i++) {
+        res = (i%3+i%5===0?"fizzbuzz":(i%5===0?"buzz":(i%3===0?"fizz":i)));
+        console.log(res);
+       }
+    }
+
+   //fizzbuzz(15);
+
+    /*
+    Напишите функцию, проверяющую, являются ли две строки анаграммами
+    друг друга (регистр букв не имеет значения). 
+    Важны только символы, пробелы или знаки препинания не учитываются. Пример:
+    anagram('finder', 'Friend')  --> true
+    anagram('hello', 'bye') --> false
+    */
+
+    function anagram(str1,str2) {
+        str1 = str1.toLowerCase().split("").sort();
+        str2 = str2.toLowerCase().split("").sort();
+        if (str1.length!==str2.length) {
+            console.log("false");
+            return;
+        }
+        str1.forEach( (v,i) => {
+            if (v!==str2[i]) {
+                console.log("false");
+                return;
+            }
+        });
+        console.log("true");
+        return;
+        
+    }
+
+    //anagram('finder', 'Friend');
+    //anagram('hello', 'bye');
+
+    /*
+    Последовательность Фибоначчи — это порядок чисел, где каждое последующее число является суммой двух предыдущих. 
+    Например, первые десять чисел последовательности выглядят так: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
+    Напишите функцию, которая возвращает n-ую запись в последовательности, где n — это число, которое вы передаёте в качестве аргумента функции.
+    Пример:
+    fibonacci(3)  // --> 2
+    */
+
+    function fibonacci(index) {
+        var res = [];
+        for (var i=0; i<=index; i++) {
+            var c = (i<=1)?i:res[i-1]+res[i-2]
+            res.push(c);
+        }
+        console.log(c);
+        return c;
+    }
+
+    //fibonacci(9);
+
+    //console.log( 20e-1['toString'](2) ); //20e-1 - это 20 * 10(в степени-1) = 20*0,1 = 2. ['toString'](2) - преобразовать в строку в двоичной системе исчесления - 2 в этой системе = 10
+    f.call(f);
+
+function f() {
+  alert( this );
+}
+    }
      
 
         
