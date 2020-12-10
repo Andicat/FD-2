69d72856312eea1572392c144c0843e7af8ea882
Katerina Andreeva <andikatya@gmail.com>
2020-12-10 20:24:27 +0300

    json2

--- dyn_form_ajax.js
+++ dyn_form_ajax.js
@@ -161,47 +161,21 @@
         });
         cntForm.appendChild(form);
     }
-
-    var URL = 'https://andicat.github.io/FD-2/';
-    var serverTime = 10000;
-    var statusOk = 200;
-    //var ajaxHandlerScript="https://fe.it-academy.by/AjaxStringStorage2.php";
-    //var stringName='Sadykov_TripleTownProject';
-    //var recordsTable;
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
+    
     function getDataJSON(fileName) {
+        var URL = 'https://andicat.github.io/FD-2/';
         var xhr = new XMLHttpRequest();
         xhr.responseType = 'text';
         xhr.addEventListener('load', function () {
             var formDef = JSON.parse(xhr.response);
             console.log(formDef);
-            //var formCnt = document.createElement("form-" + fileName);
-            //createForm(formCnt,formDef,"form" + fileName);
+            
         });
         xhr.open('GET', 'https://andicat.github.io/FD-2/' + fileName + '.json');
         xhr.send();
     };
 
     btnCreate.addEventListener('click', (event) => {
-        /*var formDef1 = [
-            {label:'Название сайта:',kind:'longtext',name:'sitename'},
-            {label:'URL сайта:',kind:'longtext',name:'siteurl'},
-            {label:'Посетителей в сутки:',kind:'number',name:'visitors'},
-            {label:'E-mail для связи:',kind:'shorttext',name:'email'},
-            {label:'Рубрика каталога:',kind:'combo',name:'division',
-            variants:[{text:'здоровье',value:1},{text:'домашний уют',value:2},{text:'бытовая техника',value:3}]},
-            {label:'Размещение:',kind:'radio',name:'payment',
-            variants:[{text:'бесплатное',value:1},{text:'платное',value:2},{text:'VIP',value:3}]},
-            {label:'Разрешить отзывы:',kind:'check',name:'votes'},
-            {label:'Описание сайта:',kind:'memo',name:'description'},
-            {label:'Опубликовать',kind:'submit'},
-        ];
-        /*debugger
-        var jjj = JSON.stringify(formDef1);
-        console.log(jjj);
         var formDef2 = [
             {label:'Фамилия:',kind:'longtext',name:'lastname'},
             {label:'Имя:',kind:'longtext',name:'firstname'},
@@ -209,14 +183,18 @@
             {label:'Возраст:',kind:'number',name:'age'},
             {label:'Зарегистрироваться',kind:'submit'},
         ];
+        var jjj = JSON.stringify(formDef2);
+        console.log(jjj);
         cntForm.innerHTML = "";
         
-        var form1 = document.createElement("form");
+        /*var form1 = document.createElement("form");
         createForm(form1,formDef1,"form1");
 
         var form2 = document.createElement("form");
         createForm(form2,formDef2,"form2");*/
-        getDataJSON("formDef1");
+        /*var formCnt = document.createElement("form-" + fileName);
+            createForm(formCnt,formDef,"form" + fileName);
+        getDataJSON("formDef1");*/
         /*var jjj = JSON.stringify(formDef1);
         console.log(jjj);*/
     });
